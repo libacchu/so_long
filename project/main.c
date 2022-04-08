e5511a18ab9e14e64289fb6baa471f264939a0d5
libacchu <libacchu@students.42wolfsburg.de>
2022-04-08 12:23:53 +0200

    create trbg

--- project/main.c
+++ project/main.c
@@ -6,20 +6,50 @@
 /*   By: libacchu <libacchu@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 13:46:22 by libacchu          #+#    #+#             */
-/*   Updated: 2022/04/03 20:41:42 by libacchu         ###   ########.fr       */
+/*   Updated: 2022/04/08 12:02:06 by libacchu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minilibx/mlx.h"
 
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}		t_data;
+
+/*
+** MEMORY OFFSET FORMULA
+** int offset = (y * line_length + x * (bits_per_pixel / 8));
+*/
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	create_trgb(int t, int r, int g, int b)
+{
+	return ()
+}
+
 int	main(void)
 {
-	void	*mlx_ptr;
+	void	*mlx;
 	void	*mlx_win;
-	void	*img;
+	t_data	img;
 
-	mlx_ptr = mlx_init();
-	mlx_win = mlx_new_window(mlx_ptr, 1920, 1080, "so_long");
-	img = mlx_new_image(mlx_ptr, 1920, 1080);
-	mlx_loop(mlx_ptr);
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "so_long");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+			&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 20, 20, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
